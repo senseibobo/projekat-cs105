7d8d64fe5cc0dc7a61b51a40a67da91ad86dd1ca
Student <student@bmu.internal>
2024-05-15 17:20:45 +0200

    NOVI UPDATE

--- script.js
+++ script.js
@@ -35,23 +35,24 @@ function processGoatAnimation() {
 
 function fillGoatList() {
   let goatList = document.getElementById("goatlist");
-  for (let goat of koze) {
-    console.log(goat);
-    goatList.innerHTML += `<div id="${goat.ime}" class="buygoatcard">
+  let kozee = JSON.parse(localStorage.getItem("koze"));
+  for (let goat of kozee) {
+    goatList.innerHTML += `
+      <div id="${goat.ime}" class="buygoatcard">
         <img src="${goat.slika}" />
         <div>
-        <h3>${goat.ime}</h3>
-        <div class="goatInfo">
-        <ul>
-        <li>Boja: ${goat.boja}</li>
-        <li>Pol: ${goat.pol}</li>
-        <li>Tezina: ${goat.tezina}</li>
-        <li>IQ: ${goat.iq}</li>
-        <li>Duzina Rogova: ${goat.duzinaRogova}</li>
-        <li>Brzina: ${goat.brzina} km/h</li>
-        </ul>
-        <button onclick="kupiKozu('${goat.ime}')">KUPI</button>
-        </div>
+          <h3>${goat.ime}</h3>
+          <div class="goatInfo">
+            <ul>
+              <li>Boja: ${goat.boja}</li>
+              <li>Pol: ${goat.pol}</li>
+              <li>Tezina: ${goat.tezina}</li>
+              <li>IQ: ${goat.iq}</li>
+              <li>Duzina Rogova: ${goat.duzinaRogova}</li>
+              <li>Brzina: ${goat.brzina} km/h</li>
+            </ul>
+            <button onclick="kupiKozu('${goat.ime}')">KUPI<br>${goat.cena}€</button>
+          </div>
         </div>
       </div>`;
   }
@@ -61,10 +62,41 @@ function kupiKozu(ime) {
   console.log(ime);
   document.getElementById(ime).getElementsByClassName("goatInfo")[0].innerHTML =
     "Koza je uspesno kupljena. Stici ce na Vasu adresu uskoro.";
+  let kozee = JSON.parse(localStorage.getItem("koze"));
+  let i = 0;
+  for (let koza of kozee) {
+    if (koza.ime == ime) {
+      kozee.splice(i, 1);
+      console.log("izbrisana koza");
+      break;
+    }
+    i++;
+  }
+  localStorage.setItem("koze", JSON.stringify(kozee));
+}
+
+function submitujKozu() {
+  console.log("submitovao sam kozu");
+  let koza = {};
+  for (let prop of [
+    "ime",
+    "boja",
+    "pol",
+    "tezina",
+    "iq",
+    "duzinaRogova",
+    "brzina",
+    "slika",
+    "cena",
+  ]) {
+    koza[prop] = document.getElementById(prop).value;
+    document.getElementById(prop).value = "";
+  }
+  dodajKozu(koza);
 }
 
 class Koza {
-  constructor(ime, boja, pol, tezina, iq, duzinaRogova, brzina, slika) {
+  constructor(ime, boja, pol, tezina, iq, duzinaRogova, brzina, slika, cena) {
     this.ime = ime;
     this.boja = boja;
     this.pol = pol;
@@ -73,10 +105,27 @@ class Koza {
     this.duzinaRogova = duzinaRogova;
     this.brzina = brzina;
     this.slika = slika;
+    this.cena = cena;
   }
 }
-let koze = [
-  new Koza("Djole", "bela", "muski", 50, 120, 15, 20, "slike/djolegoat.jpg"),
-  new Koza("Куче", "siva", "muski", 26, 67, 0, 40, "slike/kozakoza.jpg"),
-];
-localStorage.setItem("koze", koze);
+
+function dodajKozu(koza) {
+  let kozeString = localStorage.getItem("koze");
+  let kozee;
+  if (typeof kozeString == typeof "asdsad") {
+    kozee = JSON.parse(kozeString);
+  } else {
+    kozee = [];
+  }
+  kozee.push(koza);
+  localStorage.setItem("koze", JSON.stringify(kozee));
+}
+
+function napraviKoze() {
+  let koze = [
+    new Koza("Djole", "bela", "muski", 50, 120, 15, 20, "slike/djolegoat.jpg", 350),
+    new Koza("Куче", "siva", "muski", 26, 67, 0, 40, "slike/kozakoza.jpg", 3),
+  ];
+
+  localStorage.setItem("koze", JSON.stringify(koze));
+}
